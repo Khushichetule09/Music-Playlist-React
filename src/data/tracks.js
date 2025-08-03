c1b10ec8222ed7b0688c2b330173f782a188d982
khushichetule09 <khushichetule2@gmail.com>
2025-08-03 19:31:29 +0530

    Completed music player website with personal songs and animated UI

---
+++ src/data/tracks.js
@@ -0,0 +1,29 @@
+const TRACKS = [
+	{
+		title: 'Rozana - (Emotional touch music)',
+		artist: 'Sing By Khushi',
+		cover: 'https://imgs.search.brave.com/oaQlPNBoLRHqt6l1ULGpSJEA34VYgUkf94wWx2-wXjo/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/aGluZGl0cmFja3Mu/aW4vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTcvMDMvcm96YW5h/LmpwZw',
+		src: './songs/rozana.mp3',
+	},
+	// {
+	// 	title: 'Jungle Waves (Drum&Bass Electronic Inspiring Promo)',
+	// 	artist: 'DIMMYSAD',
+	// 	cover: 'https://cdn.pixabay.com/audio/2025/05/21/12-47-01-748_200x200.jpg',
+	// 	src: './songs/jungle-waves.mp3',
+	// },
+	{
+		title: 'Titli (Magic with music and nature)',
+		artist: 'Sing By Khushi',
+		cover: 'https://imgs.search.brave.com/nSFbnt9xATk0FsvFUJwo-vkR0I7XzgV6EmlnSWF5Row/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi90aXRs/aS1idXR0ZXJmbHkt/eWVsbG93LWNvbG9y/LWRlYWQtZmx5LTQ4/NzgwOTgxLmpwZw',
+		src: './songs/titli.mp3',
+	},
+	{
+		title: 'Jhol (Describing things with song)',
+		artist: 'Sing By Khushi',
+		cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNgffE23jsW3keptSWv7hGroX_sxkoV0fNTQ&s',
+		src: './songs/jhol.mp3',
+	},
+
+	];
+
+export default TRACKS;
